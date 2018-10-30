681001a1f3a7dcbd7b8b634a218b94a8f9825e9b
zunigao <zunigao@cmc306-11.mathcs.carleton.edu>
2018-10-29 22:11:44 -0500

    Exercise 2 almost complete

--- BcastReduce.java
+++ BcastReduce.java
@@ -1,6 +1,6 @@
 import mpi.*;
 import java.nio.*;
-import java.util.Scanner.*;
+import java.util.Scanner;
 import java.lang.Math.*;
 
 public class BcastReduce {
@@ -13,16 +13,16 @@ public class BcastReduce {
         int rank = MPI.COMM_WORLD.getRank();
         String name = MPI.COMM_WORLD.getName();
 
-        IntBuffer buffer = MPI.newIntBuffer(1);
+        DoubleBuffer buffer = MPI.newDoubleBuffer(1);
 
         int numItemsToTransfer = 1;
         int sourceRank = 0;
         int bufferPosition = 0;
         
 
-        int n;
-        int width = 1 / n;
-        int sum = 0;
+        double n = 1000;
+        double width  = 0.0;
+        double sum = 0.0;
 
 
     /**
@@ -39,27 +39,46 @@ public class BcastReduce {
     */
 
         if (rank == sourceRank) {
-            Scanner in = new Scanner(System.in);
-            System.out.print("n = ");
-            n = in.nextInt();
-            in.close();
+            //Scanner in = new Scanner(System.in);
+            //System.out.print("n = ");
+            //n = (double)in.nextInt();
+
+            //in.close();
             buffer.put(bufferPosition, n);
         }
 
-        MPI.COMM_WORLD.bcast(buffer, numItemsToTransfer, MPI.INT, sourceRank);
 
-        int valueReceived = buffer.get(0);
-        for (int i = 1; i <= n*(rank+1)/size; i++)
+        MPI.COMM_WORLD.bcast(buffer, numItemsToTransfer, MPI.DOUBLE, sourceRank);
+
+        double valueReceived = buffer.get(0);
+        
+        n = valueReceived;
+        //System.out.println(n);
+
+        width = 1 / n;
+        
+
+        double start = (rank / size) * n + 1;
+        double end = ((rank + 1)/size) * n ;
+
+        for (double i = start ; i <= end; i++)
         {
-            sum += 4 / (1+Math.pow(width * (i-0.5), 2));
+            
+            sum += 4 / (1+ Math.pow(width * (i-0.5), 2));
+            //System.out.println(sum);
+            
         }
-        int piFrag = width * sum;
+
+        
+        double piFrag = width * sum;
+        System.out.println(piFrag);
 
         buffer.put(bufferPosition, piFrag);
-        IntBuffer finalAnswer = MPI.newIntBuffer(1);
+        DoubleBuffer finalAnswer = MPI.newDoubleBuffer(1);
+        //System.out.println();
         int destProcess = 0;
         MPI.COMM_WORLD.reduce(buffer, finalAnswer, numItemsToTransfer,
-                              MPI.INT, MPI.SUM, destProcess);
+                              MPI.DOUBLE, MPI.SUM, destProcess);
         if (rank == 0) {
             System.out.println("Final answer = " + finalAnswer.get(0));
         }
