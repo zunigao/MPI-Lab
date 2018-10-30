a1485e8b45978f30670e9206cfd10dec1d5b60a0
zunigao <zunigao@cmc306-11.mathcs.carleton.edu>
2018-10-29 20:51:32 -0500

    Exercise 1

--- SendRecv.java
+++ SendRecv.java
@@ -10,7 +10,7 @@ public class SendRecv {
         int size = MPI.COMM_WORLD.getSize();
         int rank = MPI.COMM_WORLD.getRank();
 
-        if (size < 2) {
+        if (size < 4) {
             System.out.println("Too few processes!");
             System.exit(1);
         }
@@ -18,22 +18,33 @@ public class SendRecv {
         IntBuffer buffer = MPI.newIntBuffer(1);
 
         int numItemsToTransfer = 1;
-        int sourceRank = 0;
-        int destinationRank = 1;
+        int sourceRank = rank - 1;
+        int destinationRank = rank + 1;
         int messageTag = 0;  // supplemental tag, often just not used
         int bufferPosition = 0;
-
         if (rank == 0) {
-            int valueToTransmit = 5;
-            buffer.put(bufferPosition, valueToTransmit);
-            MPI.COMM_WORLD.send(buffer, numItemsToTransfer, MPI.INT,
-                                destinationRank, messageTag);
-        } else if (rank == 1) {
-            Status status = MPI.COMM_WORLD.recv(buffer, numItemsToTransfer,
-                                                MPI.INT, sourceRank, messageTag);
-            System.out.println("Process 1 received value " +
-                               buffer.get(bufferPosition) + " from process 0");
+                int valueToTransmit = -1;
+                buffer.put(bufferPosition, valueToTransmit);
+                sourceRank = size-1;
+                MPI.COMM_WORLD.send(buffer, numItemsToTransfer, MPI.INT,
+                                    destinationRank, messageTag);
+            } 
+        
+        if(rank == size - 1){
+            destinationRank = 0;
         }
+
+        
+        
+        Status status = MPI.COMM_WORLD.recv(buffer, numItemsToTransfer,
+                                            MPI.INT, sourceRank, messageTag);
+        System.out.println("Process " + rank + " received value " +
+                           buffer.get(bufferPosition) + " from process " + sourceRank );
+         MPI.COMM_WORLD.send(buffer, numItemsToTransfer, MPI.INT,
+                                destinationRank, messageTag);
+                
+            
+        
 
         MPI.Finalize();
     }
