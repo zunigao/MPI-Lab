0d213e15a539ccd5c184b22920225e3e882240b4
Blake Johnson <blakeconnorjohnson@gmail.com>
2018-10-29 21:22:11 -0500

    version 2 check

--- BcastReduce.java
+++ BcastReduce.java
@@ -1,5 +1,7 @@
 import mpi.*;
 import java.nio.*;
+import java.util.Scanner.*;
+import java.lang.Math.*;
 
 public class BcastReduce {
 
@@ -18,7 +20,7 @@ public class BcastReduce {
         int bufferPosition = 0;
         
 
-        int n = 1000;
+        int n;
         int width = 1 / n;
         int sum = 0;
 
@@ -37,18 +39,24 @@ public class BcastReduce {
     */
 
         if (rank == sourceRank) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("n = ");
+            n = in.nextInt();
+            in.close();
             buffer.put(bufferPosition, n);
         }
 
         MPI.COMM_WORLD.bcast(buffer, numItemsToTransfer, MPI.INT, sourceRank);
 
         int valueReceived = buffer.get(0);
+        for (int i = 1; i <= n*(rank+1)/size; i++)
+        {
+            sum += 4 / (1+Math.pow(width * (i-0.5), 2));
+        }
+        int piFrag = width * sum;
 
-        
-
-     
-
-        //IntBuffer finalAnswer = MPI.newIntBuffer(1);
+        buffer.put(bufferPosition, piFrag);
+        IntBuffer finalAnswer = MPI.newIntBuffer(1);
         int destProcess = 0;
         MPI.COMM_WORLD.reduce(buffer, finalAnswer, numItemsToTransfer,
                               MPI.INT, MPI.SUM, destProcess);
